74bfef65f4ab5b5628a19694ae631c10b577584e
harshit <harshitkumar9548@gmail.com>
2024-01-25 12:13:49 +0530

    completed till variable challanges

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("H   H K   k");
+        System.out.println("H   H K  k");
+        System.out.println("H   H Kk");
+        System.out.println("HHHHH K   ");
+        System.out.println("H   H Kk");
+        System.out.println("H   H K k");
+        System.out.println("H   H K   k");
+    }
+}
